e064d2736f8517250640c29e225c983249105823
Richard Stibbard <richardstibbard@gmail.com>
2020-05-01 21:46:21 +0100

    Cumulative total - WORKING

---
+++ 06-cumulative-total.php
@@ -0,0 +1,22 @@
+<?php
+
+/* Write some code that outputs a cumulative total of the current line number for 1 to 50.
+
+e.g.
+
+1
+1 + 2
+1 + 2 + 3
+1 + 2 + 3 + 4
+etc.
+Example output:
+
+*/
+
+$output = 0;
+
+for ($i = 1; $i <= 50; $i += 1) {
+    $output += $i;
+}
+
+var_dump($output);
