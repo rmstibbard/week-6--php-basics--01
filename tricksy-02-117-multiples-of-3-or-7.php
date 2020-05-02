df80eef362ccb9522e525b02115df99e774b7157
Richard Stibbard <richardstibbard@gmail.com>
2020-05-02 10:37:19 +0100

    Sum of first 117 multiples of 3 or 7 - WORKING

---
+++ tricksy-02-117-multiples-of-3-or-7.php
@@ -0,0 +1,19 @@
+<?php
+/* Write some code that var_dumps the sum of the first 117 multiples of either 3 or 7.
+
+e.g. 3 + 6 + 7 + 9 + 12 + 14 + 15 + 18 + 21 + 24 ...
+
+The answer should be int(16107). */
+
+$counter = 0;
+$total = 0;
+
+for ($i = 1; $i <= 400; $i += 1) {
+    if (($i % 3 === 0) || ($i % 7 === 0)) {
+        $counter += 1;
+        if ($counter <= 117) {
+            $total += $i;
+        }
+    }
+}
+var_dump($total);
