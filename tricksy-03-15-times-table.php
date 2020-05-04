7d771211c6dce14c3835bd417cfd704fc4dd9b0c
Richard Stibbard <richardstibbard@gmail.com>
2020-05-04 14:15:08 +0100

    15 times table - WORKING

--- tricksy-03-15-times-table.php
+++ tricksy-03-15-times-table.php
@@ -1,7 +1,7 @@
 <?php
 
 for ($i = 1; $i <= 15; $i += 1) {
-    $thisLine = "\n";
+    $thisLine = ($i > 1) ? "\n" : "";
     for ($j = 1; $j <= 15; $j += 1) {
         $thisLine .= ($i * $j) . "\t";
     }
