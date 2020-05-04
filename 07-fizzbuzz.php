aa64ce3e8d46c57fe1ce7fb1d73979222127403d
Richard Stibbard <richardstibbard@gmail.com>
2020-05-04 12:22:22 +0100

    Fizzbuzz - WORKING

--- 07-fizzbuzz.php
+++ 07-fizzbuzz.php
@@ -1,9 +1,10 @@
 <?php
 
-// 1. With if ... else:
+// 1. With if ... else not very good
+// as cannot easily add more words
 for ($i = 1; $i <= 100; $i += 1) {
     if (($i % 5 === 0) && ($i % 3 === 0)) {
-        echo "FizzBuzz\n";              // Cannot easily add more words
+        echo "FizzBuzz\n";
     } else if ($i % 3 === 0) {
         echo "Fizz\n";
     } else if ($i % 5 === 0) {
@@ -13,11 +14,13 @@ for ($i = 1; $i <= 100; $i += 1) {
     }
 }
 
-// 2. Better with ternary statements as can easily add more funny words (?Dan's or Kieran's answer??):
+// 2. Much better solution used ternary statements 
+//  as can easily add any number of words 
+// (Dan's or Kieran's answer):
 for ($i = 1; $i <= 100; $i += 1) {
-    $result = $i % 3 === 0 ? "Fizz\n" : "";
-    $result .= $i % 5 === 0 ? "Buzz\n" : "";
-    $result .= $i % 7 === 0 ? "Splat\n" : ""; // Optional splats and booms
-    $result .= $i % 11 === 0 ? "Boom\n" : "";
-    echo $result !== "" ? $result  : $i . "\n";
+    $result = ($i % 3 === 0) ? "Fizz\n" : "";
+    $result .= ($i % 5 === 0) ? "Buzz\n" : "";
+    $result .= ($i % 7 === 0) ? "Splat\n" : ""; // Optional splats and booms
+    $result .= ($i % 11 === 0) ? "Boom\n" : "";
+    echo ($result !== "") ? $result  : $i . "\n";
 }
